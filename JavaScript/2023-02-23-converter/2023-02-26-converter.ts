2982525f2e7ceb6b6e1cf0f5382fb1115ea3628e
lldite <lldite@gmail.com>
2023-03-23 17:54:39 +0200

    latest changes

--- JavaScript/2023-02-23-converter/2023-02-26-converter.ts
+++ JavaScript/2023-02-23-converter/2023-02-26-converter.ts
@@ -1,10 +1,11 @@
 let kilogram = document.querySelector("#kg") as HTMLInputElement
 let pound = document.querySelector("#lb") as HTMLElement
+// 1. var:
+kilogram.addEventListener("change", function (event) {
+  pound.innerHTML = kilogram.value * 2.205
+})
 
-// kilogram.addEventListener("change", function (event) {
-//   pound.innerHTML = kilogram.value * 2.205
-// })
-
+//2. var:
 function handler(event: Event): void {
   pound.innerHTML = kilogram.value * 2.205
 }
@@ -14,22 +15,25 @@ let meter = document.querySelector("#m") as HTMLInputElement
 let foot = document.querySelector("#ft") as HTMLInputElement
 let poga = document.querySelector("#button") as HTMLElement
 
-// poga.addEventListener("click", function (event) {
-//   console.log(2)
-//   foot.value = meter.value * 3.281
-// })
+// ar pogu m -> ft
+poga.addEventListener("click", function (event) {
+  console.log(2)
+  foot.value = meter.value * 3.281
+})
 
 console.log(document.querySelector("#button"))
 
-// function handleChange(event: Event): void {
-//   console.log("TADA!")
-//   if ((event.target as Element).id == "m") foot.value = meter.value * 3.281
-//   if ((event.target as Element).id == "ft") meter.value = foot.value / 3.281
-// }
+// bez pogas m <-> ft
+function handleChange(event: Event): void {
+  console.log("TADA!")
+  if ((event.target as Element).id == "m") foot.value = meter.value * 3.281
+  if ((event.target as Element).id == "ft") meter.value = foot.value / 3.281
+}
 
-// meter.addEventListener("change", handleChange)
-// foot.addEventListener("change", handleChange)
+meter.addEventListener("change", handleChange)
+foot.addEventListener("change", handleChange)
 
+// ar pogu m <-> ft
 poga.addEventListener("click", (event) => {
   let toMeter: Boolean = meter.value == ""
   let toFoot: Boolean = foot.value == ""
